01a65e0b3d402a9594632cdc3c9bd49eb790e072
raiprograming <raishivam2304@gmail.com>
2021-07-11 21:19:31 +0530

    initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgoN3C1fOUF3gHvfR5pcwYuzYHTyQbxmw",
+  databaseURL:"https://githubrealtimedb-default-rtdb.firebaseio.com/",
+  authDomain: "githubrealtimedb.firebaseapp.com",
+  projectId: "githubrealtimedb",
+  storageBucket: "githubrealtimedb.appspot.com",
+  messagingSenderId: "87436206153",
+  appId: "1:87436206153:web:c2f2c94749905d2b3ddf98",
+  measurementId: "G-GPKL37MDFG",
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
